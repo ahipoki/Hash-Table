e86cdf1fb03af10ff7925cb7ef5167395f5105d7
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-06-09 23:37:49 -0700

    Update Hashtable.cpp

--- Hashtable.cpp
+++ Hashtable.cpp
@@ -112,3 +112,167 @@ void addStudent(Node**& hashTable, int& size, int& numStudents){
     rehash(hashTable, size);
   }
 }
+
+void printStudent(Node** hashTable, int size){
+  bool students = false;
+  for (int i = 0; i < size; i++){
+    if (hashTable[i] == NULL){
+      continue;
+    }
+    Node* n = hashTable[i];
+    students = true;
+    do{
+      cout<<endl;
+      cout<<"Student: "<<n->student->firstName<<" "<<n->student->lastName<<endl;
+      cout<<"ID: "<<n->student->id<<endl;
+      cout<<"GPA: "<<n->student->id<<endl;
+      n = n->next;
+    } while (n != NULL);
+  }
+  if (!students){
+    cout<<"Student list is currently empty"<<endl;
+    cout<<endl;
+  }
+}
+
+void deleteStudent(Node**& hashTable, int size, int& numStudents){
+  Student* removeStudent = new Student();
+  cout<<"Enter student's first name"<<endl;
+  cin.getline(removeStudent->firstName, 20);
+  cin.clear();
+  cin.ignore(999, '\n');
+  cout<<"Enter student's last name"<<endl;
+  cin.getline(removeStudent->lastName, 20);
+  cin.clear();
+  cin.ignore(999, '\n');
+  cout<<"Enter student's ID"<<endl;
+  cin>>removeStudent->id;
+  cin.clear();
+  cin.ignore(999, '\n');
+  int hash = (getHash(removeStudent->firstName, size)+getHash(removeStudent->lastName, size)*3+removeStudent->id)%size;
+  Node* n = hashTable[hash];
+  Node* prev = NULL;
+  if (hashTable[hash] != NULL){
+    do{
+      if (*(n->student) == *removeStudent){
+        if (prev == NULL){
+          hashTable[hash] = n->next;
+          delete n;
+        }
+        else{
+          prev->next = n->next;
+          delete n;
+        }
+        numStudents--;
+        return;
+      }
+      prev = n;
+      n = n->next;
+    } while (n != NULL);
+  }
+  cout<<"That student could not be found"<<endl;
+  return;
+}
+
+void generateRandom(Node**& hashTable, vector<char*>* firstNames, vector<char*>* lastNames, int& numStudents, int& size){
+  cout<<"Enter the amount of students you want to generate"<<endl;
+  int numToGenerate;
+  cin>>numToGenerate;
+  cin.ignore(999, '\n');
+  if (numToGenerate <= 0){
+    cout<<"You cannot generate negative students"<<endl;
+    return;
+  }
+  if (numToGenerate > 1000){
+    cout<<"You cannot generate that many students! It will now set it to 100"<<endl;
+    numToGenerate = 100;
+  }
+  for (int i = 0; i < numToGenerate; i++){
+    Student* newStudent = new Student();
+    int random = rand()%2000;
+    strcpy(newStudent->firstName, firstNames->at(random));
+    random = rand()%1000;
+    strcpy(s->lastName, lastNames->at(random)); 
+    newStudent->id = numStudents++;
+    newStudent->gpa = float(rand()%350+100)/100;
+    int hash = (getHash(newStudent->firstName, size) + getHash(newStudent->lastName, size)*3 + newStudent->id)%size;
+    int chainlength = addChain(hashTable, hash, newStudent);
+    if (chainlength > 3 || numStudents > size/2){
+      rehash(hashTable, size);
+    }
+  }
+}
+
+int addChain(Node**& hashTable, int h, Student* s){
+  if (hashTable[h] == NULL){
+    hashTable[h] = new Node();
+    hashTable[h]->student = s;
+    return 0;
+  }
+  int chainLength = 0;
+  Node* link = hashTable[h];
+  while (link->next != NULL){
+    if (*(link->student) == *s){
+      return -1;
+    }
+    link = link->next;
+    chainLength++;
+  }
+  if (*(link->student) == *s){
+      return -1;
+  }
+  link->next = new Node();
+  link->next->student = s;
+  return chainLength;
+}
+
+void rehash(Node**& hashTable, int& size){
+  Node** tempHash = new Node*[size*2];
+  for (int i = 0; i < size*2; i++){
+    tempHash[i] = NULL;
+  }
+  for (int i = 0; i < size; i++){
+    if (hashTable[i] == NULL){
+      continue;
+    }
+    Node* n = hashTable[i];
+    do{
+      Student* s = n->student;
+      int hash = (getHash(s->firstName, size*2) + getHash(s->lastName, size*2)*3 + s->id)%(size*2);
+      addChain(tempHash, hash, s);
+      n=n->next;
+    } while (n != NULL);
+  }
+  delete[] hashTable;
+  hashTable = tempHash;
+  size *= 2;
+}
+
+int getHash(char* s, int size){
+  int hash = 3;
+  for (int i = 0; i < strlen(s); i++){
+    hash *= 71993;
+    hash += int(s[i]);
+    hash %= size;
+  }
+  hash *= 71993;
+  hash %= size;
+  return hash;
+}
+
+bool readNames(vector<char*>* names, char* firstName, int n){
+  ifstream fileStream(firstName);
+  if (!fileStream.is_open()){
+    cout<<"Couldn't read from file"<<endl;
+    return false;
+  }
+  for (int i = 0; i < n; i++){
+    char name[80];
+    fileStream.getline(name, sizeof(name));
+    if (n == 1000){
+      name[strlen(name)-1] = '\0';
+    }
+    names->push_back(name);
+  }
+  return true;
+}
